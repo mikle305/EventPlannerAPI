0c701cec8d3e35a03d8bb9a5f254a7bc3cdd4950
Mikhail <mikle3305@gmail.com>
2022-07-02 23:09:59 +0300

    Added all tables with constraints, IProject/Create

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EventPlannerAPI.Data;
+using EventPlannerAPI.Models;
+
+namespace EventPlannerAPI.Controllers;
+
+[ApiController]
+[Route("api/I[controller]")]
+public class ProjectController : Controller
+{
+    private readonly DataContext _db;
+
+    public ProjectController(DataContext db)
+    {
+        _db = db;
+    }
+    
+    [HttpPost("Create")]
+    public async Task<IActionResult> Create(int userId, string projectName)
+    {
+        var project = new Project()
+        {
+            Name = projectName,
+            CreatedAt = DateTime.Now
+        };
+        await _db.Projects.AddAsync(project);
+        await _db.SaveChangesAsync();
+        await _db.UsersProjects.AddAsync(
+            new UserProject()
+            {
+                ProjectId = project.Id,
+                UserId = userId,
+                Role = ProjectUserRole.Owner,
+                IsNotifiable = false,
+            });
+        await _db.SaveChangesAsync();
+        
+        return Ok(new
+        {
+            Success = true,
+            ProjectId = project.Id
+        });
+    }
+}
--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EventPlannerAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EventPlannerAPI.Data;
 
 public class DataContext: DbContext
 {
     public DbSet<User> Users { get; set; }
+    
+    public DbSet<Project> Projects { get; set; }
+    
+    public DbSet<UserProject> UsersProjects { get; set; }
+
+    public DbSet<Event> Events { get; set; }
 
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
@@ -14,6 +21,16 @@ public class DataContext: DbContext
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
-        builder.Entity<User>().HasIndex(u => new {u.Email, u.Username}).IsUnique();
+        builder.Entity<User>().HasIndex(user => new {user.Email, user.Username}).IsUnique();
+        // Complex pk
+        builder.Entity<UserProject>().HasKey(userProject => new { userProject.UserId, userProject.ProjectId });
+        // One to many (Users to UsersProjects)
+        builder.Entity<UserProject>()
+            .HasOne(userProject => userProject.User)
+            .WithMany(user => user.UserProjects);
+        // One to many (Projects to UsersProjects)
+        builder.Entity<UserProject>()
+            .HasOne(userProject => userProject.Project)
+            .WithMany(project => project.UsersProject);
     }
 }
